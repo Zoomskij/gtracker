a7f7db5c410f053803de9670662108e128d41e18
Потапов Артем Олегович <potapov.ao@rmsauto.local>
2017-09-27 17:41:41 +0300

    added answers

--- main.go
+++ main.go
@@ -15,4 +15,21 @@ func main() {
 
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	upd, _ := bot.GetUpdatesChan(ucfg)
+	for {
+		select {
+		case update := <-upd:
+			ChatID := update.Message.Chat.ID
+			Text := update.Message.Text
+
+			if Text == "/go" {
+				msg := tgbotapi.NewMessage(ChatID, "go")
+				bot.Send(msg)
+			}
+
+		}
+	}
 }
